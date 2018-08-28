418591012b32fa58beeabffe255b51ae0668ce60
Matthew Etress <mattetress@me.com>
2018-08-28 21:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var katzDeliLine = [];
-
+var line = [];
 function takeANumber(lineLength, custName) {
   let line = [];
   line.push(custName);
-  
-  return line, 
-  
-  return `Welcome, ${custName}. You are number ${lineLength + 1} in line.`;
-} 
+  return line; }
+  return `Welcome, ${line[line.length -1]}. You are number ${line.length} in line.`;
