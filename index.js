86979f233ec9004821bd8e2b4141d0a8257efd56
Matthew Etress <mattetress@me.com>
2018-08-28 21:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(line, custName) {
   line.push(custName);
   return `Welcome, ${custName}. You are number ${line.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    let next = katzDeliLine[0];
+    katzDeliLine.shift();
+    return next;
+  }
+}
