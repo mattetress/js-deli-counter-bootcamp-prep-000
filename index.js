6bb7e8a5f2efef8a593b3bf75bdc6f1e002847c6
Matthew Etress <mattetress@me.com>
2018-08-28 21:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var katzDeliLine = [];
+
 function takeANumber(lineLength, custName) {
-  
+  katzDeliLine.push(custName);
   return `Welcome, ${custName}. You are number ${lineLength + 1} in line.`;
 } 
