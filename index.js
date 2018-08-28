343691ec43e6df2e817c67ebbda636d2438d8a92
Matthew Etress <mattetress@me.com>
2018-08-28 21:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${next}.`;
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    let list = [];
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      list.push(`${i + 1}. ${list[i]}`); 
+    } return `The line is currently: ${...list}`;
+  }
+}
